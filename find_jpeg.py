b8f644dac238558a2198c88bb248a3fe00bf3edd
theinr <theinr@taltech.ee>
2025-02-11 13:26:51 +0200

    py for the win

---
+++ find_jpeg.py
@@ -0,0 +1,20 @@
+import os
+
+# loon funktsiooni, mis kontrollib kas fail on .jpeg, lugedes headerist 2 esimest bitti
+def picture(file_path):
+    with open(file_path, 'rb') as file:
+        header = file.read(2)
+        return header == b'\xFF\xD8'
+
+# määran directory, kus asuvad failid aka random_files
+directory = 'C:\\Users\\TarvoHeinroos\\PycharmProjects\\scriptinglanguages\\searching-for-jpeg-files-TarvoH\\random_files'
+
+# kontrollin igat faili directory's ning kui on .JPEG siis prindin välja, vastasel juhul kustutan
+for file in os.listdir(directory):
+    file_path = os.path.join(directory, file)
+    if os.path.isfile(file_path):
+        if picture(file_path):
+            print(f'{file} is JPEG')
+        else:
+            os.remove(file_path)
+            print(f'{file} deleted')
